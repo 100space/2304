585429bbeea0f522d75835b5c38cc1075f839b77
BAEK'space <112856532+100space@users.noreply.github.com>
2023-04-28 09:09:23 +0900

    add : 0428 init

---
+++ 0428/__tests__/crypto/crypto.test.ts
@@ -0,0 +1,81 @@
+import { GENESIS } from "@constasnts/block.constants"
+import { BlockData, BlockInfo } from "@core/block/block.interface"
+import CryptoModule from "@core/crypto/crypto.module"
+
+describe("CryptoModule", () => {
+    let cryptoModule: CryptoModule
+    beforeEach(() => {
+        cryptoModule = new CryptoModule()
+    })
+    describe("SHA256", () => {
+        it("SHA256에 인자 내용을 평문으로해서 암호화가 되는가?", () => {
+            const data = "123"
+            const result = cryptoModule.SHA256(data)
+            expect(result.length).toBe(64)
+        })
+    })
+    describe("createBlockHash", () => {
+        it("SHA256에서 blockinfo 데이터로 암호화가 진행되는가?", () => {
+            //blockinfo 넣기전에 data 속성을 빼기
+            const blockinfo: BlockData = {
+                version: GENESIS.version,
+                height: GENESIS.height,
+                timestamp: GENESIS.timestamp,
+                previousHash: GENESIS.previousHash,
+                merkleRoot: GENESIS.merkleRoot,
+                nonce: GENESIS.nonce,
+                difficulty: GENESIS.difficulty,
+                data: "",
+            }
+            //객체 -> blockInfo(data 제외)
+            const hash = cryptoModule.createBlockHash(blockinfo)
+            console.log(hash)
+            expect(hash).toHaveLength(64)
+        })
+    })
+    describe("HashtoBinary", () => {
+        it("이진데이터로 잘 변환되는가?", () => {
+            const data = "hash"
+            const hash = cryptoModule.SHA256(data)
+            const binary = cryptoModule.hashToBinary(hash)
+            expect(binary.length).toBe(256)
+        })
+    })
+    describe("merkleroot", () => {
+        it("genesis 블럭에 있는 data값에서 merkleroot 구하기", () => {
+            const merkleroot = cryptoModule.merkleRoot(GENESIS.data)
+            expect(merkleroot).toHaveLength(64)
+        })
+        it("data값이 TransactionRow[] 일 경우 merkleroot가 잘 생성되는가?", () => {
+            const data = [
+                { hash: "DC24B19FB7508611ACD8AD17F401753670CFD8DD1BEBEF9C875125E98D82E3D1" },
+                { hash: "DC24B19FB7508611ACD8AD17F401753670CFD8DD1BEBEF9C875125E98D123314" },
+            ]
+            const merkleroot = cryptoModule.merkleRoot(data)
+            expect(merkleroot).toHaveLength(64)
+        })
+        it("data값이 올바르지 않은 경우 에러가 발생하는가?", () => {
+            const data = [
+                { hash: "DC24B19FB7508611ACD8AD17F401753670CFD8DD1BEBEF9C875125E98D82E3DG" },
+                { hash: "DC24B19FB7508611ACD8AD17F401753670CFD8DD1BEBEF9C875125E98D12331G" },
+            ]
+            expect(() => {
+                cryptoModule.merkleRoot(data)
+            }).toThrowError()
+        })
+    })
+    describe("isValidHash", () => {
+        it("hash length가 64 미만인 경우", () => {
+            const hash = "DC24B19FB7508611ACD8AD17F401753670CFD8DD1BEBEF9C875125E98D82E3D" // hash값이 틀려야 통과를 함.
+            expect(() => {
+                cryptoModule.isValidHash(hash)
+            }).toThrowError()
+        })
+        it("hash 값이 64글자이지만 올바르지 않은 형식일경우 (마지막에 G 넣음)", () => {
+            const hash = "DC24B19FB7508611ACD8AD17F401753670CFD8DD1BEBEF9C875125E98D82E3DG" // hash값이 틀려야 통과를 함.
+            expect(() => {
+                cryptoModule.isValidHash(hash)
+            }).toThrowError()
+        })
+    })
+})
---
+++ 0428/src/index.ts
@@ -0,0 +1,35 @@
+import { GENESIS } from "@constasnts/block.constants"
+import Block from "@core/block/block"
+import { IBlock } from "@core/block/block.interface"
+import ProofOfWork from "@core/block/workproof/proofofwork"
+import WorkProof from "@core/block/workproof/workproof"
+import CryptoModule from "@core/crypto/crypto.module"
+
+console.log("hello bitcoin")
+
+//블럭을 100~1000개 만들기
+
+const crypto = new CryptoModule()
+const proofofwork = new ProofOfWork(crypto)
+const workProof = new WorkProof(proofofwork)
+const block = new Block(crypto, workProof)
+
+// const block1 = block.createBlock(GENESIS, "123123", GENESIS)
+// const block2 = block.createBlock(block1, "123123", GENESIS)
+// const block3 = block.createBlock(block2, "123123", GENESIS)
+
+// console.log(block1, 1)
+// console.log(block2, 2)
+// console.log(block3, 3)
+
+let blockArr = [GENESIS]
+let Nblock: IBlock
+let previousBlock = GENESIS
+let adjustmentBlock = GENESIS
+for (let i = 1; i < 100; i++) {
+    previousBlock = blockArr[i - 1]
+    if (i > 19) adjustmentBlock = blockArr[Math.floor(i / 10 - 1) * 10]
+    Nblock = block.createBlock(previousBlock, "123123124124", adjustmentBlock)
+    blockArr.push(Nblock)
+}
+console.log(blockArr)
