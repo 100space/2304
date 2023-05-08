2cb9ef2cd8c78300a9ad84f547709c9672fe79e5
BAEK'space <112856532+100space@users.noreply.github.com>
2023-05-08 12:52:09 +0900

    add : 0508 4교시

--- 0508/src/wallet_front/app.ts
+++ 0508/src/wallet_front/app.ts
@@ -1,14 +1,27 @@
 import express from "express"
 import nunjucks from "nunjucks"
 import axios from "axios"
+import path from "path"
+import Wallet from "@core/wallet/wallet"
 
-export default () => {
+export default (accounts: Wallet) => {
     const app = express()
+    const viewDir = path.join(__dirname, "views")
     app.use(express.json())
     app.set("view engine", "html")
-    nunjucks.configure("views", {
+    nunjucks.configure(viewDir, {
         express: app,
     })
-
+    app.get("/", (req, res) => {
+        res.render("index")
+    })
+    app.post("/wallet", (req, res) => {
+        const account = accounts.create()
+        res.json(account)
+    })
+    app.get("/wallet", (req, res) => {
+        const accountList = accounts.getAccounts()
+        res.json(accountList)
+    })
     return app
 }
--- 0508/src/wallet_front/index.ts
+++ 0508/src/wallet_front/index.ts
@@ -1,5 +1,11 @@
+import CryptoModule from "@core/crypto/crypto.module"
+import DigitalSignature from "@core/wallet/digitalSignature"
+import Wallet from "@core/wallet/wallet"
 import WalletClient from "@wallet_front/app"
-const app = WalletClient()
+const crypto = new CryptoModule()
+const digitalSignature = new DigitalSignature(crypto)
+const accounts = new Wallet(digitalSignature)
+const app = WalletClient(accounts)
 
 app.listen(3000, () => {
     console.log(`wallet Start`)
