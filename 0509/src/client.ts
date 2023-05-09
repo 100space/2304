ff6c6e7979af176abef316ed3564a8c5be872136
BAEK'space <112856532+100space@users.noreply.github.com>
2023-05-09 10:34:46 +0900

    add : 0509 2교시(1)

---
+++ 0509/src/client.ts
@@ -0,0 +1,5 @@
+import P2PNetwork from "@server/p2p"
+
+const p2p = new P2PNetwork()
+p2p.listen(8556)
+p2p.connet(8555, "127.0.0.1")
--- 0509/src/server/p2p.ts
+++ 0509/src/server/p2p.ts
@@ -1,9 +1,12 @@
 import net, { Socket } from "net"
 
 class P2PNetwork {
-    listne(port: number) {
+    listen(port: number) {
         const server = net.createServer((socket: Socket) => {
-            console.log(socket)
+            console.log(socket.remotePort)
+            console.log("connection")
+
+            socket.write("hello??")
         })
         console.log("p2p server start")
         server.listen(port)
@@ -11,7 +14,13 @@ class P2PNetwork {
     connet(port: number, host: string) {
         const socket = new net.Socket()
         socket.connect(port, host, () => {
+            console.log(socket.remotePort)
             console.log("connect")
+
+            socket.on("data", (data: Buffer) => {
+                console.log(data)
+                console.log(data.toString("utf-8")) //Buffer을 UTF-8 글자셋으로 변환해준다.
+            })
         })
     }
 }
