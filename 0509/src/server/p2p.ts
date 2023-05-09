0d534c39494798b2e6bfefa06ad8dc8bc22546d3
BAEK'space <112856532+100space@users.noreply.github.com>
2023-05-09 11:03:15 +0900

    add : 0509 2교시

--- 0509/src/server/p2p.ts
+++ 0509/src/server/p2p.ts
@@ -1,12 +1,21 @@
+import { IBlock } from "@core/block/block.interface"
 import net, { Socket } from "net"
+import { MessageData } from "./network.interface"
 
 class P2PNetwork {
     listen(port: number) {
         const server = net.createServer((socket: Socket) => {
             console.log(socket.remotePort)
             console.log("connection")
-
             socket.write("hello??")
+
+            socket.on("data", (data: Buffer) => {
+                console.log(data.toString("utf-8"))
+                const messageString = data.toString("utf-8")
+
+                const message = JSON.parse(messageString)
+                console.log(message)
+            })
         })
         console.log("p2p server start")
         server.listen(port)
@@ -20,6 +29,12 @@ class P2PNetwork {
             socket.on("data", (data: Buffer) => {
                 console.log(data)
                 console.log(data.toString("utf-8")) //Buffer을 UTF-8 글자셋으로 변환해준다.
+                const message: MessageData = {
+                    type: "latestBlock",
+                    payload: {} as IBlock,
+                }
+                const messageString = JSON.stringify(message)
+                socket.write(messageString)
             })
         })
     }
