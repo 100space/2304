5a31da460be75fbf323814a3153b0493367fbc7c
BAEK'space <112856532+100space@users.noreply.github.com>
2023-05-09 12:02:01 +0900

    add : 0509 3교시

--- 0509/src/server/p2p.ts
+++ 0509/src/server/p2p.ts
@@ -1,4 +1,5 @@
 import { IBlock } from "@core/block/block.interface"
+import { Handler } from "express"
 import net, { Socket } from "net"
 import { MessageData } from "./network.interface"
 
@@ -20,9 +21,25 @@ class P2PNetwork {
         console.log(`[+] New Connection from ${socket.remoteAddress}:${socket.remotePort}`)
         this.sockets.push(socket)
         // 브로드케스트
+
+        this.dataHandler(socket)
+
+        const message: MessageData = {
+            type: "latestBlock",
+            payload: {} as IBlock,
+        }
+        socket.write(JSON.stringify(message))
+
         const disconnect = () => this.handleDisconnect(socket)
         socket.on("close", disconnect)
         socket.on("error", disconnect)
+    }
+
+    private dataHandler(socket: Socket) {
+        socket.on("data", (data: Buffer) => {
+            const { type, payload } = JSON.parse(data.toString("utf-8"))
+            console.log(type, payload)
+        })
     }
 
     private handleDisconnect(socket: Socket) {
